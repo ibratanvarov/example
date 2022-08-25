9402874565f01f0f06d05d1c918bf51b1e0fb9be
Ibrat Anvarov <anvarovibrat@gmail.com>
2022-08-25 21:58:04 +0500

    it is about index function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function  index(){
+        $b = 0;
+        $a = $b;
+
+        return $a;
+    }
 }
